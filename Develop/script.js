1b2611b45afd7e3d63af7081df195f58e975279c
Carolene Wilson <carolenesw@gmail.com>
2020-05-19 16:40:08 -0400

    set time vale to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -18,6 +18,27 @@ var time3 = $(".time3");
 var time4 = $(".time4");
 var time5 = $(".time5");
 
+var time9 = $(".time9").append("<p>9:00 am</p>").val("4:00 am")
+localStorage.setItem("time9", JSON.stringify(time9))
+
+console.log(time9)
+
+var time9E = JSON.parse(localStorage.getItem("time9"))
+console.log(time9E[0]);
+// console.log(time9E.value[0])
+
+// console.log(typeof "time9E")
+console.log('Vals', time9E[0])
+// console.log((time9E).val())
+
+if(time9E === "4:00 am") {
+    alert("perfect")
+}
+else {
+    alert("wrong time") 
+}
+
+
 // stringify timeSlots to use in calendar time log 
 var availableTimes = timeSlots.toString();
 console.log(availableTimes)
