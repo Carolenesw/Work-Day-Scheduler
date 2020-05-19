4c5f13c810644e9b6755bf0100054997b6dc0050
Carolene Wilson <carolenesw@gmail.com>
2020-05-19 14:01:44 -0400

    create variables for current time slots

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,21 @@
 $(document).ready(function () {
 
     // use moment.js to add current time and append to page
-
    var currentTime = moment().format('LLLL');
    console.log(currentTime)
-   
    $("#currentDay").append(currentTime);
+
+//    create variables 
+var time9 = $(".time9");
+var time10 = $(".time10");
+var time11 = $(".time11");
+var time12 = $(".time12");
+var time1 = $(".time1");
+var time2 = $(".time2");
+var time3 = $(".time3");
+var time4 = $(".time4");
+var time5 = $(".time5");
+
+
+
 })
