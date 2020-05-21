2007b5a3bb8e8e3114fb95f241787f4023be72a9
Carolene Wilson <carolenesw@gmail.com>
2020-05-21 13:16:33 -0400

    create if esle statement for comparison

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,9 @@ $(document).ready(function () {
     var currentTime = moment().format('LLLL');
     console.log(currentTime)
 
-
     // create variables to use to compare times for coloring 
     var timeSlots = (moment().format('h:00 A'));
     console.log(timeSlots)
-    console.log(typeof "timeSlots");
 
     // stringify timeSlots to use in calendar time log 
     var availableTimes = timeSlots.toString();
@@ -23,23 +21,28 @@ $(document).ready(function () {
     // var startTime = moment(year + '-' + month + '-' + 01 + ' 00:00').format('h:00 A')
     console.log(startTime)
 
-    var time9 = moment().add(22, "hours").format('h:00 A');
+    startTime = moment().add(21, "hours").format('h:00 A');
+    console.log(startTime)
+    var time9 = moment().add(20, "hours").format('h:00 A');
     console.log(time9)
     // use momentjs to increment time by 1 hour each for timeslots
-    var time10 = moment().add(23, "hours").format('h:00 A');
-    var time11 = moment().add(24, "hours").format('h:00 A');
-    var time12 = moment().add(25, "hours").format('h:00 A');
-    var time1 = moment().add(26, "hours").format('h:00 A');
-    var time2 = moment().add(27, "hours").format('h:00 A');
-    var time3 = moment().add(28, "hours").format('h:00 A');
-    var time4 = moment().add(29, "hours").format('h:00 A');
-    var time5 = moment().add(30, "hours").format('h:00 A');
+    var time10 = moment().add(21, "hours").format('h:00 A');
+    var time11 = moment().add(22, "hours").format('h:00 A');
+    var time12 = moment().add(23, "hours").format('h:00 A');
+    var time1 = moment().add(24, "hours").format('h:00 A');
+    var time2 = moment().add(25, "hours").format('h:00 A');
+    var time3 = moment().add(26, "hours").format('h:00 A');
+    var time4 = moment().add(27, "hours").format('h:00 A');
+    var time5 = moment().add(28, "hours").format('h:00 A');
 
     // create array to hold time for comparison and set to local storage 
     var timeCompare = [time9, time10, time11, time12, time1, time2, time3, time4, time5];
     console.log(timeCompare[0])
     localStorage.setItem("time", timeCompare);
     console.log(timeCompare[0])
+    for (var i = 0; i < timeCompare.length; i++) {
+        console.log("good morning, it is " + timeCompare[i])
+    }
 
     // append current time and increment time to time slots on page 
     $("#currentDay").append(currentTime);
@@ -77,70 +80,72 @@ $(document).ready(function () {
 
 
 
-    // function colorBlocks() {
-    //     // var timeNow = parseInt(timeSlots)
-    //     // var timeComp = parseInt(timeCompare)
-    //     // console.log(timeNow)
-    //     // console.log(timeComp)
-    //     for (var i = 0; i < timeCompare.length; i++) {
-    //         console.log(timeCompare[i])
+    // add input data to local storage when save button is click
+    $(".save").on("click", function (event) {
+        // var textData = $(this).val()
+        // console.log(textData)
 
-    //         if (timeCompare === availableTimes) {
-    //             $(".time9").attr("style", "background-color: red")
-    //         } else if (timeCompare < availableTimes) {
-    //             $(".time9").attr("style", "background-color: grey")
-    //         } else {
-    //             $(".time9").attr("style", "background-color: green")
-    //         }
+        var textArea = $(".text").val().trim()
 
+        console.log(textArea)
+        // console.log(textData)
 
+        if (textArea === "") {
+            alert("Please enter data")
+        }
 
-    //     }
-    // }
+        // set items to local storage 
+        localStorage.setItem("text", textArea)
+        // localStorage.setItem("text9", JSON.stringify(time9));
 
-})
+    })
 
+    renderData()
+    function renderData() {
+        var textInfo = localStorage.getItem("text");
 
-// add input data to local storage when save button is click
-$(".save").on("click", function (event) {
-    // var textData = $(this).val()
-    // console.log(textData)
+    }
+    colorBlocks()
+    // create function to show color block 
+    function colorBlocks() {
+        // get times from local storage for color blocks
+        // var timeS = localStorage.getItem("time");
+        // console.log(timeS[0])
+        console.log(timeCompare)
+        if (timeSlots === timeCompare[0]) {
+            console.log(timeCompare[0])
+            $(".first-time").attr("style", "background-color: red")
+        } else if (timeSlots < timeCompare[0]) {
+            $(".first-time").attr("style", "background-color: green")
+        } else {
+            $(".first-time").attr("style", "background-color: grey")
+        }
 
-    var textArea = $(".text").val().trim()
+        if (timeSlots === timeCompare[1]) {
+            $(".second-time").attr("style", "background-color: red")
+        } else if (timeSlots < timeCompare[1]) {
+            $(".second-time").attr("style", "background-color: green")
+        } else {
+            $(".second-time").attr("style", "background-color: grey")
+        }
 
-    console.log(textArea)
-    // console.log(textData)
+        //  create loop to loop through times
+        // for (var i = 0; i < timeS.length; i++) {
+        //     console.log("good morning, it is" + timeS[i])
+        // }
 
-    if (textArea === "") {
-        alert("Please enter data")
+        //  use switch statement for color block coding
+        // switch (timeSlots) {
+        //     case timeCompare[0]:
+        //         $(".first-time").attr("style", "background-color: red");
+        //     case timeCompare[0]:
+        //         $(".first-time").attr("style", "background-color: green");
+        //     default:
+        //         $(".first-time").attr("style", "background-color: grey");
+        // }
+
     }
 
-    // set items to local storage 
-    localStorage.setItem("text", textArea)
-    // localStorage.setItem("text9", JSON.stringify(time9));
-
 })
 
-renderData()
-function renderData() {
-    var textInfo = localStorage.getItem("text");
 
-}
-colorBlocks()
-// create function to show color block 
-function colorBlocks() {
-    // get times from local storage for color blocks
-    var timeS = localStorage.getItem("time");
-    console.log(timeS[0])
-    // timeS.toString();
-    //  create loop to loop through times
-    // for (var i = 0; i < timeS.length; i++) {
-    //     console.log("good morning, it is" + timeS[i])
-    // }
-
-    //  use switch statement for color block coding
-//     switch (timeSlots) {
-//         case timeCompare[0]:
-//             $(".first-time").attr("style", "background-color: red");
-//     }
-}
